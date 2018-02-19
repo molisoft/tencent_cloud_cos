0574ebebbfa6f5a64ca89bdd063eecdd4490d68a
moli <molisoft@qq.com>
2018-02-19 10:57:44 +0800

    add file name

--- lib/tencent_cloud_cos/api.rb
+++ lib/tencent_cloud_cos/api.rb
@@ -11,15 +11,17 @@ module TencentCloudCos
       config.method = 'put'
       config.uri    = filename
       content_type  = config.content_type
+      src_filename = "attachment; filename*=\"UTF-8''#{File.basename(filename)}\""
 
       authorization = Authorization.new(config)
       begin
         response = RestClient.put("#{config.host}#{config.uri}",
                                   file,
-                                  Authorization: authorization.auth_header, 
-                                  host: config.host, 
+                                  Authorization: authorization.auth_header,
+                                  host: config.host,
                                   content_length: file.size,
-                                  content_type: content_type)
+                                  content_type: content_type,
+                                  content_disposition: src_filename)
       rescue RestClient::ExceptionWithResponse => e
         return e.response
       else
